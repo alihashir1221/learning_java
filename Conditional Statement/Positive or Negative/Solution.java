4936154f623569f1d867b94f05a746cbc568c2a9
alihashir1221 <alihashir1221@gmail.com>
2022-10-02 23:33:51 +0530

    fixed error

--- Conditional Statement/Positive or Negative/Solution.java
+++ Conditional Statement/Positive or Negative/Solution.java
@@ -1,19 +1,14 @@
 // solutions for negative or positive number
 import java.util.*;
-//Write a Java program to get a number from the user and print whether it is positive or negative. Go to the editor
-
-        //Test Data
-       // Input number: 35
-       // Expected Output :
-       // Number is positive
        class HelloWorld {
             public static void main(String[] args) {
-                Scanner sc = new Scanner(System.in);
-                int number = sc.nextInt();
-                if (number >= 0) {
-                    System.out.println("Positive");
-                }  else {
-                    System.out.println("Invalid");
+                try (Scanner sc = new Scanner(System.in)) {
+                    int number = sc.nextInt();
+                    if (number >= 0) {
+                        System.out.println("Positive");
+                    }  else {
+                        System.out.println("Invalid");
+                    }
                 }
             }
         }
