03c487961d4fa9e8674b766c8cbd797c5607a390
alihashir1221 <alihashir1221@gmail.com>
2022-10-02 23:56:40 +0530

    Updates

--- Conditional Statement/Positive or Negative/Solution.java
+++ Conditional Statement/Positive or Negative/Solution.java
@@ -1,14 +1,15 @@
 // solutions for negative or positive number
 import java.util.*;
        class HelloWorld {
+            // solutions for negative or positive number
             public static void main(String[] args) {
-                try (Scanner sc = new Scanner(System.in)) {
-                    int number = sc.nextInt();
-                    if (number >= 0) {
-                        System.out.println("Positive");
-                    }  else {
-                        System.out.println("Invalid");
-                    }
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Enter a number");
+                int number = sc.nextInt();
+                if (number > 0) {
+                    System.out.println("Positive");
+                } else {
+                    System.out.println("Negative");
                 }
             }
         }
