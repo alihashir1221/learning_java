cd877ff2e4cfcc810ef38eafa70464dece67a7e0
alihashir1221 <alihashir1221@gmail.com>
2022-10-16 23:15:16 +0530

    Solutions Added

---
+++ Conditional Statement/Convert into words/Solutions.java
@@ -0,0 +1,40 @@
+package Conditional Statement.Convert into words;
+
+public class Solutions {
+    public static void main(String[] args) {
+        System.out.println("Enter a number");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        switch (num) {
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            case 4:
+                System.out.println("Four");
+                break;
+            case 5:
+                System.out.println("Five");
+                break;
+            case 6:
+                System.out.println("Six");
+                break;
+            case 7:
+                System.out.println("Seven");
+                break;
+            case 8:
+                System.out.println("Eight");
+                break;
+            case 9:
+                System.out.println("Nine");
+                break;
+            default:
+                System.out.println("Invalid Number");
+        }
+    }
+}
