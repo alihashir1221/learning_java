7e0e3a260307fb4c98bc25da22466d04b0d03e4c
alihashir1221 <alihashir1221@gmail.com>
2022-10-19 19:46:11 +0530

    Solutions Added

---
+++ Java Loops/Print Hello World/Solutions.java
@@ -0,0 +1,7 @@
+public class Solutions {
+public static void main(String[] args) {
+    for(int counter = 0; counter < 100; counter = counter++) {
+        System.out.println("Hello World");
+    }
+ }
+}
