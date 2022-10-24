045dc1e8adbf6684b46d0152d729f750d88c718c
alihashir1221 <alihashir1221@gmail.com>
2022-10-24 10:38:03 +0530

    Soultion added

---
+++ Java Loops/Print number 1-10/Solutions.java
@@ -0,0 +1,9 @@
+package Java Loops.Print number 1-10;
+
+public class Solutions {
+    public static void main(String[] args) {
+		for (int counter=0; counter < 11; counter++){
+		    System.out.println(counter);
+		}
+	}
+}
