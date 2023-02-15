2fefeb3ab1755119a42a8a56b66824e5ddee7b7e
Ali Hashir <alihashir1221@gmail.com>
2023-02-15 21:43:09 +0530

    Solution Added

---
+++ Java Array/Array through for loop/Solutions.java
@@ -0,0 +1,10 @@
+//package Java Array.Array through for loop;
+
+public class Solutions {
+    public static void main(String args[]){  
+        int arr[]={33,3,4,5};  
+        //printing array using for-each loop  
+        for(int i:arr)  
+        System.out.println(i);  
+        }
+}
